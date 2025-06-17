bc9856f67377ed2d55c5c1c3f805340c958fa496
afsana <afsanakp420@gmail.com>
2025-06-17 17:46:47 +0530

    firshjhjjv

---
+++ evenorodd.py
@@ -0,0 +1,12 @@
+# sum of two numb
+print("enter two numbers")
+a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# c=a+b
+# print("sum of two numbers is",c)  
+
+if a%2==0:
+    print("its even number")
+else:
+    print("its odd number")   
+
